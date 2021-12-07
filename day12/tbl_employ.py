3343df08112f6a7f8773d478728f53767d06256a
reina91kr <jjangyunji@gmail.com>
2021-12-07 09:05:03 +0900

    'day13'

--- day12/tbl_employ.py
+++ day12/tbl_employ.py
@@ -1,24 +1,66 @@
 import sqlite3
+def getconn():                                  #반복을 막기 위해 함수 활용 가능
+    conn = sqlite3.connect("c:/pydb/mydb.db")
+    return conn
 
 def select_emp():
-    conn = sqlite3.connect("c:/pydb/mydb.db")   #db 연결 시작 / 객체 생성
-    cur = conn.cursor()                         #db 작업 객체
-    sql = "SELECT * FROM employee"
+    # conn = sqlite3.connect("c:/pydb/mydb.db")   #db 연결 시작 / 객체 생성
+    conn = getconn()
+    cur = conn.cursor()                         #db 작업(삽입, 삭제, 추가, 변경) 객체
+    sql = "SELECT * FROM employee ORDER BY emp_id"  #오름차순 (ASC 생략됨)
     cur.execute(sql)                            #db 작업 실행 (검색)
-    rs = cur.fetchall()                         #db 모든 자료 가져오기
+    rs = cur.fetchall()                         #db 모든 자료 가져오기 (result set)
     # print(rs)
     for i in rs:
         print(i)
     conn.close()                                #db 연결 종료
 
 def insert_emp():
-    conn = sqlite3.connect("c:/pydb/mydb.db")                           #db 연결 객체 생성
+    conn = getconn()                                                    #db 연결 객체 생성
     cur = conn.cursor()                                                 #db 작업 객체
-    sql = "INSERT INTO employee VALUES ('e1003','손흥민',30,50000)"      #data 추가
-    cur.execute(sql)
+    #입력방법 1 칼럼값을 직접 입력
+    #sql = "INSERT INTO employee VALUES ('e1006','손흥민',30,50000)"      #data 추가
+    #입력방법 2 * 더 많이 사용하는 방법
+    sql = "INSERT INTO employee VALUES (?, ?, ?, ?)"      #data 추가
+    cur.execute(sql, ('e1002','추신수',34,40000))
     conn.commit()                                                       #커밋 실행 - 필수
     conn.close()                                                        #커밋 종료 - 필수
 
-# insert_emp()
-select_emp()
+def select_one():
+    conn = getconn()
+    cur = conn.cursor()
+    #방법 1
+    #sql = "SELECT emp_id, name FROM employee WHERE salary = 40000"
+    #방법 2
+    sql = "SELECT name,salary FROM employee WHERE emp_id = ?"
+    cur.execute(sql,('e1004',)) # 튜플이기 때문에 1개를 명시할 때 콤마(,) 명시해야 함
+    rs = cur.fetchone()     #한명의 자료를 가져올때
+    print(rs)
+    conn.close()
+
+
+def update_emp():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "UPDATE employee SET age = ? WHERE emp_id = ? "
+    cur.execute(sql, (33, 'e1005'))
+    conn.commit()
+    conn.close()
+
+def del_emp():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "DELETE FROM employee WHERE emp_id = ?"
+    cur.execute(sql, ('e1004',))
+    cur.execute(sql, ('e1006',))
+    conn.commit()
+    conn.close()
+
+if __name__ =="__main__":
+    #insert_emp()
+    select_emp()
+    # select_one()
+    update_emp()
+    # del_emp()
+
 
---
+++ day13/tbl_book.py
@@ -0,0 +1,88 @@
+#book 테이블 만들기
+import sqlite3
+
+def getconn():
+    con = sqlite3.connect("./output/sample.db")
+    return con
+
+def create_table():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = """
+    CREATE TABLE book(
+        book_no     INTEGER     PRIMARY KEY         AUTOINCREMENT,
+        title       TEXT        NOT NULL,  
+        publisher   TEXT        NOT NULL, 
+        page        INTEGER
+    ) 
+    """
+    #AUTOINCREMENT - 자동 순번 (sequence)
+    cur.execute(sql)
+    conn.commit()
+    print("book 테이블 생성")
+    conn.close()
+
+def insert_book():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "INSERT INTO book (title, publisher, page) VALUES ('웹 표준의 정석', '고경희', 600)"
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
+
+#AUTOINCREMENT이기 때문에 맨 앞에 숫자 넣어주지 않음
+
+def select_book():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM book"
+    cur.execute(sql)
+    rs = cur.fetchall()     #리스트로 변환
+    # print(rs)               #리스트로 출력
+    for i in rs :
+        print(i)
+    conn.close()
+
+def del_book():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "DELETE FROM book WHERE book_no=?"
+    cur.execute(sql, ('1',))
+    conn.commit()
+    conn.close()
+
+def insert_book():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "INSERT INTO book (title, publisher, page) VALUES (?, ?, ?)"
+    cur.execute(sql, ('점프 투 파이썬', '박응용', 300))
+    conn.commit()
+    conn.close()
+
+def select_one():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM book WHERE book_no = ?"        #책 번호로 검색
+    cur.execute(sql, (1,))
+    rs = cur.fetchone()
+    print(rs)
+    conn.close()
+
+def update_book():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "UPDATE book SET page = ? WHERE book_no =? "
+    cur.execute(sql, (300, 4))
+    conn.commit()
+    conn.close()
+
+if __name__=="__main__":
+    #create_table()
+    # insert_book()
+    # del_book()
+    # insert_book()
+    select_book()
+    # select_one()
+    # update_book()
+
+
